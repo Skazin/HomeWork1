3b5d6ce31a921a0af8dff849122fc65ff81093dc
Skazin <van00746@gmail.com>
2020-09-25 18:18:00 +0300

    1.4 HM done

--- HomeWork14.java
+++ HomeWork14.java
@@ -4,17 +4,18 @@ public class HomeWork14 {
 
     public static void main(String[] args) {
 
-
-        for (boolean a = true; boolean b = true){
-         if (sleepIn(a, b)) System.out.println("Пора на работу");
-         else System.out.println("Спим дальше");}
+        sleepIn(true, false);
 
     }
 
     public static boolean sleepIn(boolean weekday, boolean vacation) {
 
-        if (!weekday || vacation) return true;
-        else return false;
+        if (!weekday || vacation) {
+            System.out.println("Спим дальше");
+            return true;}
+        else {
+            System.out.println("Пора на работу");
+            return false;}
     }
 
 }
